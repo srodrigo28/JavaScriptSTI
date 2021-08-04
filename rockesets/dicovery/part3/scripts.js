6103429d6009b6f6243f6559da9b121d9f083f59
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2021-08-04 05:44:57 -0300

    finalizando aula 4 discovery rocketseat

--- rockesets/dicovery/part3/scripts.js
+++ rockesets/dicovery/part3/scripts.js
@@ -112,10 +112,8 @@ const DOM = {
 
 const Utils = {
     formatAmount(value) {
-        // value = Number(value) * 100
-        // return value
-        value = value * 100
-        return Math.round(value)
+        value = Number(value) * 100
+        return value
     },
     formatDate(date) {
         const splittedDate = date.split("-")
